6d7cfed7939a0939c702858caa311e64b2f81262
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-16 23:36:48 +0530

    login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  userName=""
+  password=""
+
+  readValues=()=>{
+    let data={
+      "userName":this.userName,
+      "password":this.password
+    }
+
+    console.log(data)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
